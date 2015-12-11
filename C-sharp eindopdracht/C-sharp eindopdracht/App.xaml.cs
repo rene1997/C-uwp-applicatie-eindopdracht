fb18ec5271ccf729ec8aa30222a94305319d54f0
ren_keijzer <jcekeijze@avans.nl>
2015-12-11 13:38:23 +0100

    push

--- C-sharp eindopdracht/C-sharp eindopdracht/App.xaml.cs
+++ C-sharp eindopdracht/C-sharp eindopdracht/App.xaml.cs
@@ -46,7 +46,7 @@ namespace C_sharp_eindopdracht
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
--- C-sharp eindopdracht/C-sharp eindopdracht/MainPage.xaml.cs
+++ C-sharp eindopdracht/C-sharp eindopdracht/MainPage.xaml.cs
@@ -26,5 +26,7 @@ namespace C_sharp_eindopdracht
         {
             this.InitializeComponent();
         }
+
+
     }
 }
