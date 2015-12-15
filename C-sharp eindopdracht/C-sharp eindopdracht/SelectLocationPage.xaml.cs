31d2d08f57b2faac78a95f519a6caa49e19f8f45
ren_keijzer <jcekeijze@avans.nl>
2015-12-15 11:00:50 +0100

    mooi

--- C-sharp eindopdracht/C-sharp eindopdracht/SelectLocationPage.xaml.cs
+++ C-sharp eindopdracht/C-sharp eindopdracht/SelectLocationPage.xaml.cs
@@ -13,28 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace C_sharp_eindopdracht
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SelectLocationPage : Page
     {
-        public MainPage()
+        public SelectLocationPage()
         {
             this.InitializeComponent();
         }
 
-        private void fromTextbox_Click(object sender, RoutedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Frame.Navigate(typeof(SelectLocationPage));
+            
         }
 
-        private void toTextbox_Click(object sender, RoutedEventArgs e)
+        private void backButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(SelectLocationPage));
+            Frame.Navigate(typeof(MainPage));
         }
     }
 }
