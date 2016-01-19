f211e869d556531aa967af37e8586fe566fda4ce
Jeroentjuh99 <jeroenvanos2@gmail.com>
2016-01-19 16:14:32 +0100

    ik ben nu vastgelopen

--- C-sharp eindopdracht/C-sharp eindopdracht/pages/MapPage.xaml.cs
+++ C-sharp eindopdracht/C-sharp eindopdracht/pages/MapPage.xaml.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
+using Windows.Services.Maps;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
@@ -19,10 +21,36 @@ namespace C_sharp_eindopdracht.pages
     public sealed partial class MapPage : Page
     {
         private ObservableCollection<Leg> list { get; set; }
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Geolocator locator;
+        private Geoposition position;
+        MapIcon user = new MapIcon();
 
         public MapPage()
         {
             this.InitializeComponent();
+            MapView.MapElements.Add(user);
+
+            locator = new Geolocator()
+            {
+                ReportInterval = 1500
+            };
+            locator.DesiredAccuracy = PositionAccuracy.High;
+            locator.PositionChanged += Locator_PositionChanged;
+
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Locator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            if (args.Position != null)
+                this.position = args.Position;
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            UpdateMap();
         }
 
         private void AppBarButton_Click(object sender, RoutedEventArgs e)
@@ -54,6 +82,31 @@ namespace C_sharp_eindopdracht.pages
         private void Setpushpin()
         {
 
+        }
+
+        private async Task<MapRouteFinderResult> Getroute()
+        {
+            try
+            {
+                List<Geopoint> geolist = new List<Geopoint>();
+                geolist.Add(position.Coordinate.Point);
+
+                return await MapRouteFinder.GetWalkingRouteFromWaypointsAsync(geolist);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void RefreshMapLocation()
+        {
+            if (position != null)
+            {
+                MapView.Center = position.Coordinate.Point;
+                user.Location = position.Coordinate.Point;
+                user.NormalizedAnchorPoint = new Point(0.5, 0.5);
+            }
         }
     }
 }
